6bffd6a005abaa6c34bbcaf8b34344f274f2f1cf
random <random@victor-Bug.technicolor.net>
2025-01-17 12:35:10 +0100

    back to blank

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { PicturesComponent } from './pictures/pictures.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from "./banner/banner.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent,CategoriesComponent,PicturesComponent,FooterComponent],
+  imports: [HeaderComponent, CategoriesComponent, PicturesComponent, FooterComponent, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
